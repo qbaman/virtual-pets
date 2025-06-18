e953a0f32e354facd17d09dd954feeb27c9017b4
aman <78668489+qbaman@users.noreply.github.com>
2025-06-18 21:27:01 +0000

    feature: add console title and welcome screen
    - Set console Title to "Virtual Pet Simulator"
    - Added welcome message
    - Added press any key to exit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace VirtualPetSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Title = "=== Virtual Pet Simulator ===";
+            WriteLine("Welcome to the pet simulator!");
+
+            WriteLine("Press any key to exit...");
+            ReadKey();
+        }
+    }
+}
