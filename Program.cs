abab04d9c1561550b1f34dc28d37fe3ea0645e18
aman <78668489+qbaman@users.noreply.github.com>
2025-06-21 09:21:32 +0000

    feat: add multiple VirtualPet objects with names
    + Instantiated leoTheCat and juniorTheParrot
    + Assigned names via FullName field
    + Displayed details for each pet in console

--- Program.cs
+++ Program.cs
@@ -17,15 +17,21 @@ namespace VirtualPetSimulator
 ");
 
             WriteLine("Welcome to the pet simulator!");
+            WriteLine("");
 
             // First pet
             VirtualPet leoTheCat = new VirtualPet();
             leoTheCat.FullName = "Leo";
             WriteLine("> Pet 1");
             WriteLine($"My name is {leoTheCat.FullName}.");
+            WriteLine("");
 
             // Second pet
             VirtualPet juniorTheParrot = new VirtualPet();
+            juniorTheParrot.FullName = "Junior";
+            WriteLine("> Pet 2");
+            WriteLine($"My name is {juniorTheParrot.FullName}.");
+            WriteLine("");
 
             WriteLine("Press any key to exit...");
             ReadKey();
