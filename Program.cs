dbb24fcd5d04284bcdc4b10a779240ed10929dff
aman <78668489+qbaman@users.noreply.github.com>
2025-06-24 18:37:56 +0000

    feat: add VirtualPet class and first objects
    + Created new class VirtualPet
    + Instantiated objects 'leo' and 'junior'
    + Added ASCII art banner and constructor comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using virtual_pets;
 using static System.Console;
 
 namespace VirtualPetSimulator
@@ -8,7 +9,21 @@ namespace VirtualPetSimulator
         static void Main(string[] args)
         {
             Title = "=== Virtual Pet Simulator ===";
+
+            WriteLine(@"
+ __      __   _ _        _   _      _   
+ \ \    / /__| | |    __| | | |_ _ (_)__ 
+  \ \/\/ / -_) | |   (_-<   |  _| '_/ _ \
+   \_/\_/\___|_|_|   /__/    \__|_| \___/
+");
+
             WriteLine("Welcome to the pet simulator!");
+
+            // VirtualPet   = a new type (class)
+            // leo          = identifier (object name)
+            // new VirtualPet() = constructs a VirtualPet object
+            VirtualPet leo = new VirtualPet();
+            VirtualPet junior = new VirtualPet();
 
             WriteLine("Press any key to exit...");
             ReadKey();
---
+++ VirtualPet.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace virtual_pets
+{
+    internal class VirtualPet
+    {
+    }
+}
